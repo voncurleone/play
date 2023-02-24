9e48fc65fdec64c8fc5c36d85f482ee384046b35
Tim <voncurleone@gmail.com>
2023-02-23 20:20:48 -0500

    Refactored login and added load.

--- app/controllers/TaskList2.scala
+++ app/controllers/TaskList2.scala
@@ -5,10 +5,14 @@ import play.api.mvc.{AbstractController, ControllerComponents}
 import javax.inject.Inject
 
 class TaskList2 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  def login = Action { implicit request =>
+  def load = Action { implicit request =>
     val userNameOption = request.session.get("username")
     userNameOption.map { username =>
       Ok("there is session")
-    }.getOrElse(Ok("no session"))
+    }.getOrElse(Ok(views.html.main2(routes.TaskList2.login.toString)))
+  }
+
+  def login = Action { implicit request =>
+    Ok(views.html.login2())
   }
 }
