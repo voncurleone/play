622181ddf964820f83c5b7237cc3236660a5ae14
Tim <voncurleone@gmail.com>
2023-02-25 17:41:01 -0500

    Added validateLogin, remove, and mark js functions.

--- public/javascripts/version2.js
+++ public/javascripts/version2.js
@@ -1,5 +1,29 @@
 //routes
 const loginRoute = $("#login-route").val();
 const currentRoute = $("#current-route").val();
+const validateRoute = $("#validate-route").val();
+
+//csrf token
+const csrfToken = $("#csrf-token").val();
 
 $("#contents").load(currentRoute);
+
+function validateLogin() {
+  console.log("validating")
+  const username = $("#login-name").val();
+  const password = $("#login-pass").val();
+
+  $.post(validateRoute,
+    {username, password, csrfToken},
+    data => {
+      $("#contents").html(data);
+    });
+}
+
+function remove(index) {
+  console.log("removing: " + index)
+}
+
+function mark(index) {
+  console.log("marking: " + index)
+}
